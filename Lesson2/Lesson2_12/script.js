25804ad75146742355796dec65d2b81464dfa9cb
tanyadanylo <danylotanya11@gmail.com>
2026-02-22 21:03:28 +0200

    Add solution for lesson 2

---
+++ Lesson2/Lesson2_12/script.js
@@ -0,0 +1,27 @@
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+if (coursesAndDurationArray[0].monthDuration > 5) {
+    console.log("Super");
+}
+if (coursesAndDurationArray[1].monthDuration > 5) {
+    console.log("Super for Java Complex");
+}
+if (coursesAndDurationArray[2].monthDuration > 5) {
+    console.log("Super for Python Complex");
+}
+if (coursesAndDurationArray[3].monthDuration > 5) {
+    console.log("Super for QA Complex");
+}
+if (coursesAndDurationArray[4].monthDuration > 5) {
+    console.log("Super for FullStack");
+}
+if (coursesAndDurationArray[5].monthDuration > 5) {
+    console.log("Super for Frontend");
+}
