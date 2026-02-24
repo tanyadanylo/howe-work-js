e8cf59596904e0de8715718f5ae9801b0505375f
tanyadanylo <danylotanya11@gmail.com>
2026-02-24 16:10:05 +0200

    Add solution for lesson 2

---
+++ Lesson2/Lesson2.1_2.11/script.js
@@ -0,0 +1,152 @@
+// // task 1
+// let numbers = [22, true, 44, 3.14, "name", 1, ["key"], 0.5, null, 72];
+// console.log(numbers);
+// console.log(numbers[0]);
+// console.log(numbers[1]);
+// console.log(numbers[2]);
+// console.log(numbers[3]);
+// console.log(numbers[4]);
+// console.log(numbers[5]);
+// console.log(numbers[6]);
+// console.log(numbers[7]);
+// console.log(numbers[8]);
+// console.log(numbers[9]);
+//
+// // task 2
+// let book1 = {
+//     title: "Chemistry", pageCount: 289, genre: "school",
+// }
+// let book2 = {
+//     title: "Maths", pageCount: 358, genre: "school",
+// }
+// let book3 = {
+//     title: "PI", pageCount: 180, genre: "school",
+// }
+// console.log(book1, book2, book3);
+//
+// //task 3
+// let book4 = {
+//     title: "Language", pageCount: 489, genre: "school", authors: [{name: "Maria Pero", age: 42}],
+// }
+// let book5 = {
+//     title: "Lit", pageCount: 347, genre: "school", authors: [{name: "Maria Pero", age: 42}],
+// }
+// let book6 = {
+//     title: "Art", pageCount: 205, genre: "school", authors: [{name: "Katya Star", age: 42}]
+// }
+// console.log(book4, book5, book6);
+//
+// //task 4
+//
+// let users = [
+//     {name: "Maria Pero", username: "MariaPero", password: "28575622"},
+//     {name: "Karya Star", username: "KStar", password: "72492478"},
+//     {name: "Vlad Nemo", username: "NemoV11", password: "78514269"},
+//     {name: "Lilo Vas", username: "Lilo777", password: "25223219"},
+//     {name: "Michael Krud", username: "Krudmch", password: "14187956"},
+//     {name: "Anya Sas", username: "Ani21", password: "85562148"},
+//     {name: "Oleh Adrysh", username: "Oleshkaaa", password: "45424877"},
+//     {name: "Vasya Kozak", username: "VasyaKzk", password: "52515466"},
+//     {name: "Kat Daz", username: "kd875", password: "2123258947"},
+//     {name: "Teti la Monte", username: "tetilam5", password: "84158320"},
+// ]
+// console.log(users[0].password);
+// console.log(users[1].password);
+// console.log(users[2].password);
+// console.log(users[3].password);
+// console.log(users[4].password);
+// console.log(users[5].password);
+// console.log(users[6].password);
+// console.log(users[7].password);
+// console.log(users[8].password);
+// console.log(users[9].password);
+//
+// // task 5
+//
+// let temperature = [
+//     {day: "San", morning: 12, lunchtime: 17, evening: 11},
+//     {day: "Mon", morning: 15, lunchtime: 21, evening: 14},
+//     {day: "Tue", lunchtime: 15, evening: 11},
+//     {day: "Wed", morning: 9, lunchtime: 12, evening: 8},
+//     {day: "Thu", morning: 7, lunchtime: 11, evening: 5},
+//     {day: "Fri", morning: 10, lunchtime: 14, evening: 9},
+//     {day: "Sat", morning: 8, lunchtime: 12, evening: 9},
+// ]
+// console.log(temperature);
+//
+// //task 6
+// x = -3;
+// if (x !== 0) {
+//     console.log("right");
+// } else {
+//     console.log("wrong");
+// }
+//
+// //task 7
+// time = prompt("Enter current time");
+// if (time >= 0 && time <= 15) {
+//     console.log("1 quoter");
+// } else if (time > 15 && time <= 30) {
+//     console.log("2 quoter");
+// } else if (time > 30 && time <= 45) {
+//     console.log("3 quoter");
+// } else if (time > 45 && time <= 60) {
+//     console.log("4 quoter");
+// }
+//
+// //task 8
+//
+// day = prompt("Enter current date");
+// if (day > 1 && day <= 10) {
+//     console.log("first");
+// } else if (day > 10 && day <= 20) {
+//     console.log("second");
+// } else if (day > 20 && day <= 31) {
+//     console.log("third");
+// }
+//
+// // task 9
+// let day = prompt("Enter current day");
+// switch (day) {
+//     case "Sunday":
+//         alert("meet with friends");
+//         break;
+//     case "Monday":
+//         alert("Maths and Dance");
+//         break;
+//     case "Tuesday":
+//         alert("English");
+//         break;
+//     case "Wednesday":
+//         alert("Gym");
+//         break;
+//     case "Thursday":
+//         alert("English");
+//         break;
+//     case "Friday":
+//         alert("Dance");
+//         break;
+//     case "Saturday":
+//         alert("Speaking club");
+//         break;
+// }
+//
+// //task 10
+// let numberOne = prompt("Enter number");
+// let numberTwo = prompt("Enter number");
+//
+// if (numberOne > numberTwo) {
+//     console.log(numberOne);
+// } else if (numberOne < numberTwo) {
+//     console.log(numberTwo);
+// } else {
+//     console.log("numbers are equal");
+// }
+//
+// // task 11
+// let t = null;
+// if (!t) {
+//     alert(t = 'default');
+// }
+
+alert(1/0);
