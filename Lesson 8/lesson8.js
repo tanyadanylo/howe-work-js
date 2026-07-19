e091ce13b05a9be0ea20831383c154ef73d4c6d3
tanyadanylo <danylotanya11@gmail.com>
2026-07-19 13:19:59 +0300

    Add addition for lesson 8

---
+++ Lesson 8/lesson8.js
@@ -0,0 +1,11 @@
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+let newArray = coursesAndDurationArray.map((course, index) => ({id: index + 1, ...course}));
+console.log(newArray);
